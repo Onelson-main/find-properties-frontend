b80acd3a4e039cab26981dd50bc8621de888a7ff
onelson-main <onelson.main@gmail.com>
2024-12-18 06:07:07 +0100

    feat: responsive styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { useEffect, useState } from 'react'
-import './App.css'
 import { getPropertiesFromAPI } from './services/property_service';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
@@ -52,11 +51,11 @@ function App() {
       {loading ? <p className='text-center'>Loading..</p>
         : <>
           {properties.length > 0
-            ? <>
+            ? <div className='flex flex-col md:flex-row md:justify-between md:flex-wrap gap-4'>
               {properties.map((p) => {
                 return (
-                  <div key={p.id} className='bg-BACKGROUND text-FOREGROUND overflow-hidden rounded-md mb-1 border border-FOREGROUND flex flex-row items-stretch'>
-                    <img src={p.imageUrl} alt="" className='w-20 sm:w-32 md:w-48' />
+                  <div key={p.id} className='bg-BACKGROUND text-FOREGROUND overflow-hidden rounded-md mb-1 border border-FOREGROUND flex flex-row md:flex-col md:w-72 items-stretch'>
+                    <img src={p.imageUrl} alt="" className='w-20 sm:w-32 md:w-72' />
                     <div className='flex flex-1 flex-col justify-between text-left p-2 py-1'>
                       <div className='flex flex-col'>
                         <h5 className='font-bold text-base sm:text-lg'>{p.name}</h5>
@@ -64,12 +63,12 @@ function App() {
                       </div>
                       <div className='flex flex-row justify-between'>
                         <p className={`' rounded-md text-sm p-0.5 px-3 text-BACKGROUND ${p.propertyType == 'House' ? 'bg-ACCENT' : 'bg-SECONDARY'} w-min'`}>{p.propertyType}</p>
-                        <p className='block md:hidden font-bold text-right'>{p.price}</p>
+                        <p className='block sm:hidden md:block font-bold text-right'>{p.price}</p>
                       </div>
                     </div>
-                    <p className='hidden md:block ml-auto p-2 self-center text-2xl font-bold text-right'>{p.price}</p>
+                    <p className='hidden sm:block md:hidden ml-auto p-2 self-center text-2xl font-bold text-right'>{p.price}</p>
                   </div>)
-              })}</>
+              })}</div>
             : <p className='text-center'>no properties found</p>
           }</>}
       <div className="flex flex-row justify-between items-center font-bold">
